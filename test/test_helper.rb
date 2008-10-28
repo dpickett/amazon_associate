44fe2f5bf7fcda8df962c2ff6127d363be9fe60c
Dan Pickett <dpickett@enlightsolutions.com>
2008-10-28 16:06:30 -0400

    removing irrelevant commentary

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__) , '..', 'lib'))
 require 'amazon'
 
 Amazon::Ecs.configure do |options|
-  # I had to remove the options[:response_group] setting because
-  # "Large" isn"t a valid response group for browse_node_lookups
   options[:aWS_access_key_id] = ""
 end
 
