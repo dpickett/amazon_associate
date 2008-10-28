0fb56f1493197dc3dfb1d3da80a6d42106e1a0fd
Dan Pickett <dpickett@enlightsolutions.com>
2008-10-28 16:04:50 -0400

    removing associate id

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,6 @@ require 'amazon'
 Amazon::Ecs.configure do |options|
   # I had to remove the options[:response_group] setting because
   # "Large" isn"t a valid response group for browse_node_lookups
-  options[:aWS_access_key_id] = "05S9H7883ANCJCTF15G2"
+  options[:aWS_access_key_id] = ""
 end
 
