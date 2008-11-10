9453ebedab9701ec2630937169a8e54fc54dbb8b
Dan Pickett <dpickett@enlightsolutions.com>
2008-11-10 18:42:27 -0500

    again trying to clean things up

--- lib/amazon/cache_factory.rb
+++ lib/amazon/cache_factory.rb
@@ -1,28 +1,30 @@
-class Amazon::CacheFactory
-  def self.cache(request, response, strategy)
-    strategy_class_hash[strategy].cache(request, response)
-  end
-  
-  def self.initialize_options(options)
-    #check for a valid caching strategy
-    unless self.strategy_class_hash.keys.include?(options[:caching_strategy])
-      raise Amazon::ConfigurationError, "Invalid caching strategy" 
+module Amazon
+  class CacheFactory
+    def self.cache(request, response, strategy)
+      strategy_class_hash[strategy].cache(request, response)
     end
-    strategy_class_hash[options[:caching_strategy]].initialize_options(options)
-  end
   
-  def self.get(request, strategy)
-    strategy_class_hash[strategy].get(request)
-  end
+    def self.initialize_options(options)
+      #check for a valid caching strategy
+      unless self.strategy_class_hash.keys.include?(options[:caching_strategy])
+        raise Amazon::ConfigurationError, "Invalid caching strategy" 
+      end
+      strategy_class_hash[options[:caching_strategy]].initialize_options(options)
+    end
   
-  def self.sweep(strategy)
-    strategy_class_hash[strategy].sweep
-  end
+    def self.get(request, strategy)
+      strategy_class_hash[strategy].get(request)
+    end
   
-  private
-  def self.strategy_class_hash
-    {
-      :filesystem => Amazon::CachingStrategy::Filesystem
-    }
+    def self.sweep(strategy)
+      strategy_class_hash[strategy].sweep
+    end
+  
+    private
+    def self.strategy_class_hash
+      {
+        :filesystem => Amazon::CachingStrategy::Filesystem
+      }
+    end
   end
 end
--- lib/amazon/caching_strategy/base.rb
+++ lib/amazon/caching_strategy/base.rb
@@ -1,20 +1,22 @@
 #abstract class
-module Amazon::CachingStrategy
-  class Base
-    def self.cache(request, response)
-      raise "This method must be overwritten by a caching strategy"
-    end
+module Amazon
+  module CachingStrategy
+    class Base
+      def self.cache(request, response)
+        raise "This method must be overwritten by a caching strategy"
+      end
     
-    def self.initialize_options(options)
-      raise "This method must be overwritten by a caching strategy"
-    end
+      def self.initialize_options(options)
+        raise "This method must be overwritten by a caching strategy"
+      end
     
-    def self.get(request)
-      raise "This method must be overwritten by a caching strategy"
-    end
+      def self.get(request)
+        raise "This method must be overwritten by a caching strategy"
+      end
     
-    def self.sweep
-      raise "This method must be overwritten by a caching strategy"
+      def self.sweep
+        raise "This method must be overwritten by a caching strategy"
+      end
     end
   end
 end
