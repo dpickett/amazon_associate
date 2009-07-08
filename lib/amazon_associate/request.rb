43d447965f0733bbd6a743c6e5ea54f21f302249
Dan Pickett <dpickett@enlightsolutions.com>
2009-07-07 20:26:38 -0400

    modify copyright

--- lib/amazon_associate/request.rb
+++ lib/amazon_associate/request.rb
@@ -9,7 +9,7 @@ rescue LoadError
 end
 
 #--
-# Copyright (c) 2006 Herryanto Siatono, Pluit Solutions
+# Copyright (c) 2009 Dan Pickett, Enlight Solutions
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
